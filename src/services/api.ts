d2d2b4e736d4a869a6ef1fd16db80b9822eaad64
Marcio David <159567149+MarcioDavid81@users.noreply.github.com>
2024-08-27 19:38:37 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://garcom-amigo-02cbe20b86fe.herokuapp.com/
-",
+    baseURL: "https://garcom-amigo-02cbe20b86fe.herokuapp.com",
 });
